871a90ad077cefe819a6de0f35b8cd6fbc760a7d
SL0TR <mohaiminislam94@gmail.com>
2020-06-14 00:39:30 +0600

    chore: change route folder name to router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppStyles from 'styles';
 import { AppProvider } from 'components';
-import Routes from 'routes';
+import Routes from 'router';
 
 function App() {
   return (
