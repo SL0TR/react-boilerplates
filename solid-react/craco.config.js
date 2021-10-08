8db330f3c83aa2cffe6a45288c8b96d51225622c
Md. Tasnim Alam <tasnimalamcse@gmail.com>
2021-10-09 00:21:04 +0600

    use esbuild for project building

--- solid-react/craco.config.js
+++ solid-react/craco.config.js
@@ -1,7 +1,11 @@
 const CracoLessPlugin = require('craco-less');
+const CracoEsbuildPlugin = require('craco-esbuild');
 
 module.exports = {
   plugins: [
+    {
+      plugin: CracoEsbuildPlugin,
+    },
     {
       plugin: CracoLessPlugin,
     },
