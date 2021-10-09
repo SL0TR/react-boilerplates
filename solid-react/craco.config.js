ee22d1346e9abbe2179cd555614ded40641195a3
Md. Tasnim Alam <tasnimalamcse@gmail.com>
2021-10-09 20:56:52 +0600

    Fix React not defined issue

--- solid-react/craco.config.js
+++ solid-react/craco.config.js
@@ -1,13 +1,21 @@
 const CracoLessPlugin = require('craco-less');
 const CracoEsbuildPlugin = require('craco-esbuild');
+const { ProvidePlugin } = require('webpack');
 
 module.exports = {
+  webpack: {
+    plugins: [
+      new ProvidePlugin({
+        React: 'react',
+      }),
+    ],
+  },
   plugins: [
     {
-      plugin: CracoEsbuildPlugin,
+      plugin: CracoLessPlugin,
     },
     {
-      plugin: CracoLessPlugin,
+      plugin: CracoEsbuildPlugin,
     },
   ],
 };
